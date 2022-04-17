f98b5738e77c81011ad2f2d3922eafd458f9a550
Vamsi Ampolu <vamsiampolu3000@gmail.com>
2022-04-17 18:56:33 +1000

    add a pre-commit hook

---
+++ ci/lint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+poetry run pylint {quick_python,tests}/*.py
--- quick_python/non_prims.py
+++ quick_python/non_prims.py
@@ -4,7 +4,8 @@ from typing import TypeVar, Generic
 T = TypeVar('T')
 
 class ListOperations:
-    def get(self, a: list[str], n: int) -> str:
+    def get(self, a: list[T], n: int) -> T:
         return a[n]
+
     def list_length(self, a: list[T]) -> int:
         return len(a)
