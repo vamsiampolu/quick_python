db578504488ccc2daa58742ceb72ef4ed5d49102
Vamsi Ampolu <vamsiampolu3000@gmail.com>
2022-04-17 20:21:24 +1000

    a few more list built-ins

--- quick_python/non_prims.py
+++ quick_python/non_prims.py
@@ -1,5 +1,5 @@
 """Operations on non primitives such as List, Tuple and Dictionary"""
-from typing import TypeVar, Generic
+from typing import TypeVar
 
 T = TypeVar("T")
 
@@ -10,3 +10,18 @@ class ListOperations:
 
     def list_length(self, a: list[T]) -> int:
         return len(a)
+
+    def list_slice(self, a: list[T], start: int, end: int) -> list[T]:
+        return a[start:end]
+
+    def list_delete_element(self, a: list[T], n: int) -> list[T]:
+        del a[n]
+        return a
+
+    def list_append(self, a: list[T], value: T) -> list[T]:
+        a.append(value)
+        return a
+
+    def list_insert_at_index(self, a: list[T], n: int, value: T) -> list[T]:
+        a.insert(n, value)
+        return a
--- tests/test_non_prims.py
+++ tests/test_non_prims.py
@@ -12,3 +12,24 @@ class TestListOperations:
 
     def test_length_of_list(self):
         assert lsub.list_length([1, 2, 3]) == 3
+
+    def test_slice_list(self):
+        data = [1, 2, 3, 4, 5]
+        assert lsub.list_slice(data, 1, 3) == [2, 3]
+        assert lsub.list_slice(data, 1, -1) == [2, 3, 4]
+        assert lsub.list_slice(data, -1, 0) == []
+
+    def test_delete_element(self):
+        data = [1, 2, 3, 4, 5]
+        data2 = data.copy()
+        assert lsub.list_delete_element(data, 1) == [1, 3, 4, 5]
+        assert lsub.list_delete_element(data2, -1) == [1, 2, 3, 4]
+
+    def test_append_element(self):
+        data = [1, 2, 3, 4, 5]
+        lsub.list_append(data, 6)
+        assert data == [1, 2, 3, 4, 5, 6]
+
+    def test_insert_element(self):
+        data = [1]
+        assert lsub.list_insert_at_index(data, 0, 2) == [2, 1]
