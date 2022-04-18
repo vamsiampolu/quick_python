90782c4f9c6d4bc58711037dbc518e808683be03
Vamsi Ampolu <vamsiampolu3000@gmail.com>
2022-04-18 15:15:18 +1000

    did some dict work and found out about KeysView

--- quick_python/non_prims.py
+++ quick_python/non_prims.py
@@ -1,7 +1,10 @@
 """Operations on non primitives such as List, Tuple and Dictionary"""
-from typing import TypeVar, Union, Any
+from typing import TypeVar, Union, Any, Optional
+from collections.abc import KeysView
 
 T = TypeVar("T")
+K = TypeVar("K")
+V = TypeVar("V")
 
 
 class ListOperations:
@@ -48,3 +51,28 @@ class TupleOperations:
 
     def create_unary_tuple(self, t: T) -> tuple[T]:
         return (t,)
+
+    def create_normal_tuple(self, *args: Any):
+        return args
+
+
+class DictOperations:
+    def get(self, data: dict[K, V], key: K) -> Optional[V]:
+        return data.get(key)
+
+    def get_by_index(self, data: dict[K, V], key: K) -> V:
+        return data[key]
+
+    # None is the default return value if a function does not return a value.
+    def set(self, data: dict[K, V], key: K, value: V) -> None:
+        data[key] = value
+
+    def has_key(self, data: dict[K, V], key: K) -> bool:
+        return key in data
+
+    def create_from_tuples(self, pairs: list[tuple[K, V]]) -> dict[K, V]:
+        return dict(pairs)
+
+    # no clue what the mypy type for this would be. dict_keys is the python type but does not exist in mypy.
+    def get_keys(self, data: dict[K, V]) -> KeysView:
+        return data.keys()
--- tests/test_non_prims.py
+++ tests/test_non_prims.py
@@ -1,8 +1,10 @@
 import pytest
-from quick_python.non_prims import ListOperations, TupleOperations
+from quick_python.non_prims import ListOperations, TupleOperations, DictOperations
+from collections.abc import KeysView
 
 lsub = ListOperations()
 tsub = TupleOperations()
+dsub = DictOperations()
 
 
 class TestListOperations:
@@ -74,3 +76,42 @@ class TestTupleOperations:
 
     def test_tuple_with_single_element(self):
         assert tsub.create_unary_tuple("fire") == ("fire",)
+
+    def test_rest_args_is_tuple(self):
+        assert tsub.create_normal_tuple(1, "foo", True, None) == (1, "foo", True, None)
+
+
+data = {"a": 1, "c": 3}
+
+
+class TestDictOperations:
+    def test_get_item_using_get_method(self):
+        assert dsub.get(data, "a") == 1
+        assert dsub.get(data, "b") == None
+
+    def test_get_by_index_raises_KeyError_when_key_doesnot_exist(self):
+        with pytest.raises(KeyError):
+            assert dsub.get_by_index(data, "x")
+
+    def test_get_by_index_with_existing_key(self):
+        assert dsub.get_by_index(data, "a") == 1
+
+    def test_set_value_on_dictionary(self):
+        data_copy = data.copy()
+        dsub.set(data_copy, "b", 2)
+        assert dsub.get(data_copy, "b") == 2
+
+    def test_set_update_value_for_existing_key(self):
+        data_copy = data.copy()
+        dsub.set(data_copy, "a", 5)
+        assert dsub.get_by_index(data_copy, "a") == 5
+
+    def test_has_key_in_a_dictionary(self):
+        assert dsub.has_key(data, "f") == False
+        assert dsub.has_key(data, "a") == True
+
+    def test_create_from_tuples(self):
+        assert dsub.create_from_tuples([("a", 1), ("b", 2)]) == {"a": 1, "b": 2}
+
+    def test_keys(self):
+        assert dsub.get_keys(data) == KeysView(["a", "c"])
